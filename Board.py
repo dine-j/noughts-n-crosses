81d33dfbeae8a703f37e9705a8e2db64c6d7a4f2
Amandine Jala <amandine.jala@kcl.ac.uk>
2016-12-13 15:38:06 +0000

    Create setSquare to put a piece on the board and method to print board

--- Board.py
+++ Board.py
@@ -5,4 +5,16 @@ class Board:
                        [' ', ' ', ' '],
                        [' ', ' ', ' '] ]
     
-    
+    def setSquare(self, piece, (x, y)):
+        self.matrix[x][y] = piece
+        
+    def __str__(self):
+        string = ""
+        for i in range(3):
+            for j in range(3):
+                if(self.matrix[i][j] == ' '):
+                    string += "_ "
+                else:
+                    string += self.matrix[i][j] + " "
+            string += "\n"
+        return string
