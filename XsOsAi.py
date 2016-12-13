70f513a23ecb27c86ce0460539f0ed0bce81bcd5
Amandine Jala <amandine.jala@kcl.ac.uk>
2016-12-13 16:14:11 +0000

    Add basic structure for AI

---
+++ XsOsAi.py
@@ -0,0 +1,15 @@
+from abc import ABCMeta, abstractmethod
+
+class Ai:
+    __metaclass__ = ABCMeta
+    
+    @abstractmethod
+    def play(self): pass
+
+class RandomAi(Ai):
+    
+    
+class PerfectAi(Ai):
+    
+    
+        
