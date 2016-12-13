3795c8a60dababe543c4a853cf219d1bd0eba630
Amandine Jala <amandine.jala@kcl.ac.uk>
2016-12-13 15:55:44 +0000

    Add method to check if game is set

--- Board.py
+++ Board.py
@@ -4,15 +4,32 @@ class Board:
         self.matrix = [ [' ', ' ', ' '],
                        [' ', ' ', ' '],
                        [' ', ' ', ' '] ]
+        self.last_move = (-1, -1)
+        self.last_piece = ' '
     
     def setSquare(self, piece, (x, y)):
         self.matrix[x][y] = piece
+        self.last_move = (x, y)
+        self.last_piece = piece
+        
+    def checkState(self):
+        for col in range(3):
+            if self.matrix[last_move[0]][col] != last_piece:
+                return false
+        for row in range(3):
+            if self.matrix[row][last_move[1]] != last_piece:
+                return false
+        if last_move[0] == last_move[1]:
+            for d in range(3):
+                if self.matrix[d] != last_piece:
+                    return false
+        return true
         
     def __str__(self):
         string = ""
         for i in range(3):
             for j in range(3):
-                if(self.matrix[i][j] == ' '):
+                if self.matrix[i][j] == ' ':
                     string += "_ "
                 else:
                     string += self.matrix[i][j] + " "
