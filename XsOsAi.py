4b2d2ebc29f921bce4d4af146af71ad4d0ceeb4e
Amandine Jala <amandine.jala@kcl.ac.uk>
2016-12-14 15:40:06 +0000

    Start to write AI

--- XsOsAi.py
+++ XsOsAi.py
@@ -3,13 +3,26 @@ from abc import ABCMeta, abstractmethod
 class Ai:
     __metaclass__ = ABCMeta
     
+    def __init__(self, game, player_type):
+        self.game = game
+        self.player_type = player_type
+    
     @abstractmethod
     def play(self): pass
 
+
 class RandomAi(Ai):
     
-    
+    def __init__(self, game, player_type):
+        super(self, game, player_type)
+        
+    def play(self):
+        
+   
+
 class PerfectAi(Ai):
     
+    def __init__(self, game, player_type):
+        super(self, game, player_type)
     
         
